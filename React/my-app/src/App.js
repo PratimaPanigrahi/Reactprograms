edb817e65e85ed93a67714e233743d132ff97c99
PratimaPanigrahi <148442618+PratimaPanigrahi@users.noreply.github.com>
2025-05-26 13:08:00 +0000

    Changes

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -966,3 +966,45 @@
 // }
 
 // export default App;
+
+//Form1
+import React from 'react';
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+        <select>
+  <option value="grapefruit">Grapefruit</option>
+  <option value="lime">Lime</option>
+  <option selected value="coconut">Coconut</option>
+  <option value="mango">Mango</option>
+</select>
+
+<textarea>Hello,infobyte</textarea>
+      </form>
+    );
+  }
+}
+export default NameForm;
